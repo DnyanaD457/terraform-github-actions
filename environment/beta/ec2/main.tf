e08e4595149d81f61959c3598f4b01f0273547b3
DnyanaD457 <134586537+DnyanaD457@users.noreply.github.com>
2024-07-25 13:45:39 +0530

    1st commit

---
+++ environment/beta/ec2/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-west-2" # Change this to your desired region
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0" # Use your desired AMI
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
+
+variable "instance_type" {
+  default = "t2.micro" # Specify your instance size here
+}
